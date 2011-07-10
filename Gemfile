45f99bf8846977498c8189c83471a2bf8c645283
oleg <oleg@oleg.(none)>
2011-07-10 15:29:20 +0300

    second commit

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.0.0'
 gem 'mysql'
 
 # Use unicorn as the web server
+gem 'heroku'
 gem 'unicorn'
 gem 'mongrel'
 gem 'haml'
---
+++ db/migrate/20110605111951_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.column :title, :string, :limit => 50
+      t.column :description, :text, :limit => 3000
+      t.column :author_id, :integer
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end

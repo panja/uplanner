9c1536e4703f9d3753add68f6a82f003a9ea8761
oleg <oleg@oleg.(none)>
2011-07-10 19:27:32 +0300

    fix migration

--- db/migrate/20110605111951_create_projects.rb
+++ db/migrate/20110605111951_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def self.up
     create_table :projects do |t|
       t.column :title, :string, :limit => 50
-      t.column :description, :text, :limit => 3000
+      t.column :description, :string, :limit => 3000
       t.column :author_id, :integer
       t.timestamps
     end
